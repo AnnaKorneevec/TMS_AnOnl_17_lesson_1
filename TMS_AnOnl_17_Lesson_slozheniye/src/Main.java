48339ce40cac5ce08eaf22e21c9ccd0a14578bea
Анна Корнеевец <153824079+AnnaKorneevec@users.noreply.github.com>
2023-12-16 19:02:03 +0300

    Add files via upload

---
+++ TMS_AnOnl_17_Lesson_slozheniye/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+public static void main(String[] args) {
+    int number = 5;
+    System.out.println(number);
+    number = 7;
+    System.out.println(number);
+    number = 10 + 15;
+    System.out.println(number);
+    number = 40 - 12;
+    System.out.println(number);
+    number = 15 * 15;
+    System.out.println(number);
+    number = 100 / 10;
+    System.out.println(number);
+}
+        }
